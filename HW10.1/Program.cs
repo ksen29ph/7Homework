8b7dd58105743f9511c1fb0bc13d2889a9b22d8e
ksen29ph <119218516+ksen29ph@users.noreply.github.com>
2022-12-28 20:24:41 +0300

    Add files via upload

---
+++ HW10.1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.Write("Введите количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+}
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+ Console.Write("Введите координаты позиции элемента: ");
+Console.WriteLine("Введите номер строки: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца: ");
+int b = Convert.ToInt32(Console.ReadLine());
+ if (a>m && b>n)
+ Console.Write("Такого элемента нет.");
+ else
+ {
+ object c = array.GetValue(a,b);
+ Console.WriteLine("Элемент " + c);
+ }
